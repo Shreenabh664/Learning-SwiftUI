25761801cd64bef22b3858c5e9355ba1800a2246
Shreenabh Agrawal <me@shreenabh.com>
2020-11-22 22:06:47 +0530

    Add files via upload

---
+++ Landmarks-Xcode-test/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shreenabh Agrawal on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
